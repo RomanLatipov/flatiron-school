3fc7fe5f628b70d947764f7121256af5bd399618
romanlatipov <romanlatipov@gmail.com>
2024-04-03 14:15:45 -0400

    learn state

--- phase-2/state/code/beginning/src/Item.js
+++ phase-2/state/code/beginning/src/Item.js
@@ -1,17 +1,31 @@
+import { useState } from "react";
+
 // To destructure an object within an object, separate the "nests" you want with a colon.
 // Writing `export default` before our component saves us from writing `export default [function]` at the end of the file.
 export default function Item({ item: { name, price } }) {
+  //created a state
+  const [ isInCart, setInCart ] = useState(false);
+  function handleClick() {
+    setInCart(!isInCart);
+    if (!isInCart)
+      document.querySelector("body").style.backgroundColor = "blue";
+    else
+      document.querySelector("body").style.backgroundColor = "white";
+  }
+  const buttontext = isInCart ? "Remove from cart" : "Add to cart";
+  const inCart = isInCart ? "1 item in cart" : "Not in cart";
+  console.log(buttontext);
   return (
     <li>
       {/* Since we destructured props.item, we save ourselves from writing `item.name` and `item.price`. */}
       <h2>{name} for only ${price}!</h2>
       <p>
-        <button>
-          <strong>Add to Cart</strong>
+        <button onClick={handleClick}>
+          <strong>{buttontext}</strong>
         </button>
         {/* Some manual spacing, nothing to see here... */}
         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <span>Not in Cart</span>
+        <span>{inCart}</span>
       </p>
       {/* Some manual spacing between <Item> components, nothing to see here... */}
       <br/>
